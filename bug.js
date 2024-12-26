c9e60aa079dff33ce7f542a369dd278d4bbc425e
BugHunter-X <askzenai@gmail.com>
2024-12-26 04:50:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+const requestListener = (request, response) => {
+  response.writeHead(200);
+  response.end('Hello, World!');
+};
+
+const server = http.createServer(requestListener);
+
+server.listen(8080);
+console.log('Server is running on port 8080');
+//Error: Unhandled promise rejection
+//This is a common error when using async/await without proper error handling.
